a557ebd47b1727e71fd9efa2dfc82f0ef90981a1
Vladimir Gordeev <gordeev.vladimir.v@gmail.com>
2022-06-23 23:42:53 +0300

    events Y coordinate rules in Datalog

--- queries.sql
+++ queries.sql
@@ -15,45 +15,44 @@ CREATE TABLE spawns (
 
 
 -- I need to compute ordering of the goroutines based on ancestry.
--- I can easily express what I want in Datalog:
+-- I can easily express what I want in Datalog and then
+-- write corresponding recursive SQL query.
 --
--- given facts as rows:
---
---         goroutine(_Id).
---         goroutine_ancestry(_ParentId, _ChildId).
---
---         rank_option(Id, N) :-
+--         ancestry_rank_option(Id, Rank) :-
 --                 Id = "main-goroutine",
---                 N = 0.
---         rank_option(Id, N2) :-
---                 goroutine_ancestry(ParentId, Id),
---                 rank_option(ParentId, N1),
---                 successor(N1, N2).
---         rank(Id, max<N>) :-
---                 rank_option(Id, N).
---
---
--- I can express same thing in SQL queries.
+--                 Rank = 0.
+--         ancestry_rank_option(Id, Rank2) :-
+--                 spawns(ParentId, Id),
+--                 ancestry_rank_option(ParentId, Rank1),
+--                 Rank2 = Rank1 + 1.
+--         ancestry_rank(Id, max<Rank>) :-
+--                 ancestry_rank_option(Id, Rank).
 --
 -- Inspired by Jamie Brandon way of writing datalog as recursive sql queries:
 -- https://www.scattered-thoughts.net/writing/implicit-ordering-in-relational-languages
 
-CREATE VIEW prepared_goroutines AS
+CREATE VIEW ancestry_rank AS
 WITH RECURSIVE
-        rank_option(id, n) AS (
-                SELECT 'main' AS id, 0 AS n
+        ancestry_rank_option(id, rank) AS (
+                SELECT 'main' AS id, 0 AS rank
                 UNION ALL
-                SELECT spawns.childId AS id, rank_option.n+1 AS n
+                SELECT spawns.childId AS id, ancestry_rank_option.rank+1 AS rank
                 FROM spawns
-                INNER JOIN rank_option ON rank_option.id = spawns.parentId 
-        ),
-        rank(id, n) AS (
-                SELECT rank_option.id AS id, MAX(rank_option.n) AS n
-                FROM rank_option
-                GROUP BY id
-                ORDER BY n
+                INNER JOIN ancestry_rank_option ON ancestry_rank_option.id = spawns.parentId 
         )
-SELECT * FROM rank;
+SELECT ancestry_rank_option.id AS id, MAX(ancestry_rank_option.rank) AS rank
+FROM ancestry_rank_option
+GROUP BY id
+ORDER BY rank;
+
+-- CREATE VIEW time_events AS
+-- SELECT
+--         'goroutine-start' AS type,
+--         filename,
+--         line,
+--         id
+-- FROM goroutines
+-- INNER JOIN ancestry_rank ON ancestry_rank.id = goroutines.id
 
 -- name: insert-goroutine
 INSERT INTO goroutines (id, packageName, filename, line)
